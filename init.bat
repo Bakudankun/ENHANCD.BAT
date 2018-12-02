a5f601f509119cd9b6d01a9738dcf097f0a99e86
Bakudankun <bakudankun@gmail.com>
2018-12-02 20:32:53 +0900

    Use ENHANCD_FZY for the path of fzy.exe

--- init.bat
+++ init.bat
@@ -5,6 +5,11 @@ IF NOT EXIST "%~dp0ENHANCD.BAT" (
 	EXIT /B 1
 )
 
+IF NOT DEFINED ENHANCD_FZY (
+	ECHO Environment variable ENHANCD_FZY must be set the path to fzy.exe.
+	EXIT /B 1
+)
+
 IF NOT DEFINED ENHANCD_DIR (
 	SET ENHANCD_DIR=%APPDATA%\enhancd
 	SET ENHANCD_LOG=%APPDATA%\enhancd\enhancd.log
